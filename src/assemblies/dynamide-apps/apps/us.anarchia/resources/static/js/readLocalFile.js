b69930672ac317cefab02b4270ad41d293bcb0d0
Crocker, Laramie <contact@LaramieCrocker.com>
2015-04-01 20:35:53 -0700

    Latest with mongo working, panels, and stories.

---
+++ src/assemblies/dynamide-apps/apps/us.anarchia/resources/static/js/readLocalFile.js
@@ -0,0 +1,14 @@
+function readLocalFile(e) {
+  var file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var contents = e.target.result;
+    displayContents(contents);
+  };
+  reader.readAsText(file);
+}
+
+
