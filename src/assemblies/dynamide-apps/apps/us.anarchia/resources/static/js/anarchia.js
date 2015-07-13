959991f6fe50195c1a2445afef4b0102814606c5
Crocker, Laramie <contact@LaramieCrocker.com>
2015-07-13 11:34:05 -0700

    Removed addImage, consolidated anarchia.js, removed nextid references (use GUID() now)

--- src/assemblies/dynamide-apps/apps/us.anarchia/resources/static/js/anarchia.js
+++ src/assemblies/dynamide-apps/apps/us.anarchia/resources/static/js/anarchia.js
@@ -1,20 +1,3 @@
-function getNextID(collection, resultFunction){
-   function onResult(data) {
-        resultFunction(data.nextid);
-   }
-   getNextID_impl(collection, onResult);
-}
-function getNextID_impl(collection, resultFunction){   
-   // AJAX CALL HERE :
-   $.get('/anarchia?qtype=nextid&collection='+collection,resultFunction);
-}
-function getNextID_intoSelector(collection, resultSelector){
-    function resultFunction(data) {
-        $(resultSelector).html(data.nextid);
-    }
-    // AJAX CALL HERE :
-   getNextID_impl(collection,resultFunction);
-}
 function GUID(){
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
