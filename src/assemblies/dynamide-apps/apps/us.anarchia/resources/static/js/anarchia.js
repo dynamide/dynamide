0714b9cd84b6ff81d195bccb8f455e08ed583b6a
Crocker, Laramie <contact@LaramieCrocker.com>
2015-07-13 16:20:43 -0700

    Upload, create panels, create story, reorder story, view story flow is working!

--- src/assemblies/dynamide-apps/apps/us.anarchia/resources/static/js/anarchia.js
+++ src/assemblies/dynamide-apps/apps/us.anarchia/resources/static/js/anarchia.js
@@ -5,5 +5,18 @@ function GUID(){
     });
 }
 
-
+function getQueryParams(){
+    var queryParams = [];
+    var hash;
+    var q = document.URL.split('?')[1];
+    if(q != undefined){
+        q = q.split('&');
+        for(var i = 0; i < q.length; i++){
+            hash = q[i].split('=');
+            queryParams.push(hash[1]);
+            queryParams[hash[0]] = hash[1];
+        }
+    } 
+    return queryParams;
+}
 
